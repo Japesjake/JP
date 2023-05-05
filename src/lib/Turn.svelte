ff8381ff22976d2c5a246bda2b91b6c37f98916c
japesjake <japesjake@gmail.com>
2023-05-04 21:27:53 -0400

    in the process of added win message.

--- src/lib/Turn.svelte
+++ src/lib/Turn.svelte
@@ -1,21 +1,25 @@
 <script>
     import splitSlug from '/src/lib/splitSlug.js'
     import joinSlug from '/src/lib/joinSlug.js'
-    import isWon from '/src/lib/isWon.js'
+    import whoWon from '/src/lib/whoWon.js'
     import { page } from '$app/stores';
     import changeTurn from '/src/lib/changeTurn.js'
     $: [data, turn] = splitSlug($page)
 </script>
 <h1>
-    {#if !isWon()}
+    {#if whoWon($page)}
         {#if turn == 'x'}
             &#10060
         {:else if turn == 'o'}
             &#11093
         {/if}
             's turn
-        {#if isWon()}
     {:else}
-        {changeTurn(turn)} won!
+        {#if turn == 'x'}
+            &#11093
+        {:else}    
+            &#10060
+        {/if}
+            won!
     {/if}
 </h1>
--- src/lib/whoWon.js
+++ src/lib/whoWon.js
@@ -1,8 +1,7 @@
 import splitSlug from '/src/lib/splitSlug.js'
 import { page } from '$app/stores'
 function checkCoords ($page, coords, symbol) {
-    let slug = $page.params.data;
-    let data = splitSlug(slug);
+    let [data, turn] = splitSlug($page);
     let counter = 0;
     for (let x = 0; x < data.length; x++){
         for (let y = 0; x < data[x].length; y++) {
@@ -16,9 +15,18 @@ function checkCoords ($page, coords, symbol) {
     return counter == 3;
 }
 function checkSymbol ($page, symbol){
-    checkCoords($page, coords, symbol)
+    let coords = [[0,0],[0,1],[0,2]]
+    let bool = true
+    // let bool = checkCoords($page, coords, symbol)
+    return bool
 }
-export default function isWon() {
-    checkSymbol ($page, 'x')
-    checkSymbol ($page, 'y')
+export default function whoWon($page) {
+    let bools = [true, false]
+    // let bools = [checkSymbol($page, 'x'), checkSymbol($page, 'y')]
+    if (bools[0]) {
+        return 'x'
+    } else if (bools[1]) {
+        return 'y'
+    }
+        return '_'
 }
