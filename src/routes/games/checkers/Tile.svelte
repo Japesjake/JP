fe44206b460ab18552c3a6aee7c0e92fd16d787e
japesjake <japesjake@gmail.com>
2023-05-26 21:56:09 -0400

    fixed size and placement of pieces.

--- src/routes/games/checkers/Tile.svelte
+++ src/routes/games/checkers/Tile.svelte
@@ -6,9 +6,11 @@
 </script>
 
 <div style='height: 50px; width: 50px; background-color: {tile['color']};'>
+    <div style='padding-top: 6px; padding-left: 9px; font-size: 30px'>
     {#if tile['piece'] == 'red'}
         &#128308
     {:else if tile['piece'] == 'white'}
         &#9898
     {/if}
+    </div>
 </div>
