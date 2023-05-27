a6d4ae0cd0493f7c3afb124619a629ef5e8790e3
japesjake <japesjake@gmail.com>
2023-05-26 21:05:07 -0400

    fixed issue where server was trying to access localStorage.

--- src/lib/checkers/stores.js
+++ src/lib/checkers/stores.js
@@ -2,17 +2,29 @@ import { writable } from 'svelte/store'
 import makeDefault from '/src/lib/checkers/makeDefault.js'
 import { browser } from '$app/environment'
 
-function store (key, defaultValue) {
-    if (!localStorage.getItem(key)) {
-        var value = defaultValue;
-    } else {
-        var value = JSON.parse(localStorage.getItem(key));
-    }
-    const write = writable(value);
-    return write;
-}
-
 let data = []
-let defaultValue = makeDefault(data)
-export default data = store("data", defaultValue)
-data.subscribe((value) => localStorage.data = JSON.stringify((value)))
+data = makeDefault(data)
+let initialValue = browser ? localStorage.getItem('data') ?? data : data;
+data = writable(data)
+data.subscribe((value) => {
+    if (browser) {
+        localStorage.setItem('data', value)
+    }
+})
+
+export default data;
+
+// function store (key, defaultValue) {
+//     if (!localStorage.getItem(key)) {
+//         var value = defaultValue;
+//     } else {
+//         var value = JSON.parse(localStorage.getItem(key));
+//     }
+//     const write = writable(value);
+//     return write;
+// }
+
+// let data = []
+// let defaultValue = makeDefault(data)
+// export default data = store("data", defaultValue)
+// data.subscribe((value) => localStorage.data = JSON.stringify((value)))
